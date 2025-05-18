58e3528ac0601afc412f7bc46f40a3d41d5b4d26
Winnie <wnniesich@gmail.com>
2025-05-17 20:11:55 -0700

    Finished landing page

---
+++ script.js
@@ -0,0 +1,7 @@
+// (function($) {
+//     $(document).ready(function() {
+//         $('.nav-item.dropdown').hover(function() {
+//             $(this).find('.dropdown-menu').toggleClass('show');
+//         });
+//     });
+// })(jQuery);
