531b0e133da88b29550898577a220bf273e5e420
Winnie <wnniesich@gmail.com>
2025-05-19 01:10:31 -0700

    Updated landing

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// (function($) {
-//     $(document).ready(function() {
-//         $('.nav-item.dropdown').hover(function() {
-//             $(this).find('.dropdown-menu').toggleClass('show');
-//         });
-//     });
-// })(jQuery);
+(function($) {
+    $(document).ready(function() {
+        // Nav dropdown on hover
+        $('.nav-item.dropdown').hover(function() {
+            $(this).find('.dropdown-menu').toggleClass('show');
+        });
+
+        // Google Maps
+        
+    });
+})(jQuery);
